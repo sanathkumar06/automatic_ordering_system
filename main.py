356ea3314a066c9e2093767dff62c8b680cc4aeb
prasad145 <acharyaprasad145@gmail.com>
2021-02-18 12:26:54 +0530

    firebase Authentication added

--- main.py
+++ main.py
@@ -1,18 +1,62 @@
-from flask import Flask, render_template, url_for, request
+import pyrebase
+from flask import Flask, render_template, request, redirect, session
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+#firebase credentials
+config = {
+    "apiKey" : "AIzaSyCrigjfRyiP75zMOko_WV2ZaPWfREaDtCw",
+    "authDomain" : "automatic-ordering-system.firebaseapp.com",
+    "databaseURL" : "https://automatic-ordering-system-default-rtdb.firebaseio.com",
+    "projectId" : "automatic-ordering-system",
+    "storageBucket" : "automatic-ordering-system.appspot.com",
+    "messagingSenderId" : "825591819908",
+    "appId" : "1:825591819908:web:b6859e5983040afa101cd1",
+    "measurementId" : "G-QSVE0X5VC2"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+            email = request.form['name']
+            password = request.form['password']
+            
+            try:
+                auth.sign_in_with_email_and_password(email, password)
+                return render_template('home.html')
+            except:
+                unsuccessful = 'Please check your credentials'
+                return render_template('index.html', umessage=unsuccessful)
+    return render_template('index.html')
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if (request.method == 'POST'):
+            email = request.form['name']
+            password = request.form['password']
+            auth.create_user_with_email_and_password(email, password)
+            return render_template('index.html')
+    return render_template('create_account.html')
+
+"""
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if (request.method == 'POST'):
+            email = request.form['name']
+            auth.send_password_reset_email(email)
+            return render_template('index.html')
+    return render_template('forgot_password.html')
+"""
+
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
 
-@app.route("/SignUp")
-def SignUp():
-    return render_template("signup.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
