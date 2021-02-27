132f0f7bd029af1ed0db571ab6008dcf3a1bb594
sanathkumar06 <42084450+sanathkumar06@users.noreply.github.com>
2021-02-27 22:25:06 +0530

    Inserted table3

--- dbMaker.py
+++ dbMaker.py
@@ -5,7 +5,16 @@ from datetime import datetime, date
 
 def dateConvert(d):
     sec = (d-25569)*86400
-    return str(datetime.utcfromtimestamp(sec).date())
+    day = str(datetime.utcfromtimestamp(sec).date())
+    #print(day)
+    lis = []
+    for i in day:
+        if(i=='-'):
+            lis.append('_')
+        else:
+            lis.append(i)
+    return ''.join(lis)
+
 
         
 
@@ -19,28 +28,30 @@ s = xl.sheet_by_index(0)
 num = s.nrows
 
 '''
-Table 1
+#Table 1
 create = "CREATE TABLE IF NOT EXISTS table1(stockID text PRIMARY KEY, quantity int);"
 insert = "INSERT INTO table1(stockID, quantity) values "
-
-cur.execute(create)
+try:
+    cur.execute(create)
+except:
+    print("dtabase error")
 db.commit()
 
 cnt = 0
-for i in range(0, num):
+for i in range(1, num):
     data = s.row_values(i)
     stock_id = data[1]
     quant = 0
     val = "('"+ stock_id + "' ,"+ str(quant) +");"
-    print(val)
+    #print(val)
     q = insert + val
     try:
         cur.execute(q)
     except:
         print(val)
 db.commit()
-
-Table - 2
+'''
+#Table - 2
 create = "CREATE TABLE IF NOT EXISTS table2(Price_Date Date, Price float, stockID text, FOREIGN KEY(stockID) REFERENCES table1(stockID));"
 insert = "INSERT INTO table2(Price_Date, Price, stockID) values "
 
@@ -50,8 +61,10 @@ db.commit()
 
 #print(type(data[1]))
 #print(data[2] , data[3])
+
+
 cnt = 0
-for i in range(20, num):
+for i in range(1, num):
     data = s.row_values(i)
     sec = (data[1]-25569)*86400
     priceDate = str(datetime.utcfromtimestamp(sec).date())
@@ -68,59 +81,46 @@ for i in range(20, num):
         print(val)
 db.commit()
 print(cnt)
+
 '''
-loc2 = 'table_data/stockID.xlsx'
+#Table 3
+loc2 = 'table_data/table3.xlsx'
 xl = xlrd.open_workbook(loc2)
 adr = xl.sheet_by_index(0)
 rows = adr.nrows
+header_row = adr.row_values(0)
+col = len(header_row)
 
-create = "CREATE TABLE IF NOT EXISTS table3(Sale_Date Date, "
+
+create = 'CREATE TABLE IF NOT EXISTS table3(stockID text, '
 # Price float, stockID text, FOREIGN KEY(stockID) REFERENCES table1(stockID));"
-col = ""
-ins = "table3(Sale_Date, "
-for i in range(1, rows):
-    row_num = adr.row_values(i)
-    val = str(row_num[1])
-    if i != rows - 1:
-        #ins += (val + ", ")
-        create += (val + " text, ") 
+#ins = 'table3(stockID, '
+for i in range(1, col):
+    val = dateConvert(header_row[i])
+    if i!= col-1:
+        #ins += '"'+val + '"'  + ', '
+        create += '"'+str(val) + '"'  + ' float, '
     else:
-        #ins += (val + ")")
-        create += (val + " text);")
-
-print(create)
+        #ins += '"'+val + '"' + ')'
+        create += '"'+str(val) + '"'  + 'float);'
 cur.execute(create)
 db.commit()
-#sprint(create + "\n")
-#print(ins + "\n")
 
-loc1 = 'table_data/Table3.xlsx'
-xls = xlrd.open_workbook(loc1)
-addr = xls.sheet_by_index(0)
-rowws = addr.nrows
 
-state = ""
-
-for i in range(1, rowws):
-    state += "INSERT INTO " + ins + " values " + "("
-    for j in range(0, rows):
-        if(j==0):
-            state+=str(dateConvert(addr.row_values(i)[j])) +", "
+for i in range(1,rows):
+    data = adr.row_values(i)
+    state = 'INSERT INTO table3 values (' 
+    for j in range(0, col):
+        if(j!=col-1):
+            state+=  '"'+str(data[j])+'"' +', '
         else:
-            state += (str(addr.row_values(i)[j]))
-            if(j<rows - 1):
-                state+= ", "
-    state+=");\n"
+            state += '"'+str(data[j])+'"' + ');'
     try:
         cur.execute(state)
     except:
         print(state)
-   # print(state)
+        break
+
 
 db.commit()
-
-cur.execute(create)
-db.commit()
-#data = s.row_values(1)
-
-
+'''
