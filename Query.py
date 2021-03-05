96d38573dfafdaf9b59edd1e316e883c46ccf621
prasad145 <acharyaprasad145@gmail.com>
2021-03-05 13:02:48 +0530

    added from and to dates querry

---
+++ Query.py
@@ -0,0 +1,27 @@
+import sqlite3
+from flask import Flask, request, redirect
+def query(f, t, stockID):
+    #formatedDates = []
+    with sqlite3.connect("data.db") as conn:
+        cur = conn.cursor()
+        dates = cur.execute("select invoice_date from table4 where invoice_date between :fr and :to;", {"fr" : f, "to" : t}).fetchall()
+        sales = []
+        for i in range(0, len(dates)):
+            s = str(dates[i])
+            #print(s[2:12])
+            requiredDate = s[2:12]
+            requiredDate = requiredDate.replace('-', '_')
+            requiredDate = '"' + requiredDate + '"'
+            #q = "select "+ requiredDate +" from table3 where stockID = :id;"
+            #print(q)
+            sale = cur.execute("select "+ requiredDate +" from table3 where stockID = :id;",{ "id" : stockID}).fetchone()
+            sales.append(sale);
+            #print(sales)  
+            #formatedDates.append(requiredDate)
+        print(sales)
+        final_sales = []
+        for i in range(0, len(sales)):
+            j = str(sales[i])
+            final_sales.append(float(j[1:-2]))    
+    return final_sales
+print(query("2010-12-01", "2010-12-02", "10002"))
