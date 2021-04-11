8a53e3213dc1feb210efb2d8b7fa1deb9a456dbd
Nikith Shetty <nikith.shetty@blueoptima.com>
2021-04-11 10:37:23 +0530

    Added Order Queing

--- orderManagement.py
+++ orderManagement.py
@@ -1,8 +1,13 @@
-import sqlite3
-# import Query
+import Query
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from datetime import datetime
+import json
+
+now = datetime.now()
+pathToQueue = "orderQueue.json"
+
 
 def sendMail(info, quantity):
     myMail = "auto.order.system@gmail.com"
@@ -11,7 +16,7 @@ def sendMail(info, quantity):
     msg = MIMEMultipart('alternative')
     msg['From'] = myMail
     msg['To'] = info['distributorMail']
-    #todo Vamshi
+    # todo Vamshi
     # add proper order subject and message
     # https://www.freecodecamp.org/news/send-emails-using-code-4fcea9df63f/
     msg['Subject'] = "ESCN"
@@ -24,20 +29,34 @@ def sendMail(info, quantity):
     s.send_message(msg)
     s.quit()
 
-info = {'distributorMail': "nikithks007@gmail.com"}
-sendMail(info, '5000')
 
-def placeOrder(itemID):
-    itemInfo = Query.getItemInfo(itemID)
+def repredict(itemID):
     # TODO Sanath
     orderQuantity = 000
-    sendMail(itemInfo, orderQuantity)
+    return orderQuantity
+
+
+def addToOrderQueue(itemID, quantity):
+    current_time = now.strftime("%H:%M")
+    info = Query.getItemInfo(itemID)
+    with open(pathToQueue) as f:
+        data = json.load(f)
+    data[itemID] = {"time": current_time, "name": info['name'], "quantity": quantity, "price": info['price']}
+
+    with open(pathToQueue, 'w') as outfile:
+        json.dump(data, outfile)
+
+
+addToOrderQueue('ITEM_05', 500)
 
 
 def checkAvailability(itemID):
     predictionData = Query.getItemPrediction(itemID)
     if Query.getCurrentSales(itemID) >= predictionData[itemID]:
-        placeOrder(itemID)
+        currentStock = Query.getCurrentStock(itemID)
+        newPrediction = repredict(itemID)
+        if newPrediction > currentStock:
+            addToOrderQueue(itemID, currentStock)
 
 
 def processSale(itemId, quantity):
